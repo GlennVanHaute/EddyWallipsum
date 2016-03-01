410a276638f4bd8202f8d8a59cdf2dfd790b3c93
Glenn <glenn@MacBook-Pro-van-Karlo.local>
2016-03-01 17:56:58 +0100

    starting up

--- index.php
+++ index.php
@@ -16,51 +16,66 @@
 <html>
 <head>
 	<title>Eddy Wallypsum</title>
+	<link rel="stylesheet" href="assets/css/main.css">
+	<script type="text/javascript" src="assets/js/script.js"></script>
+
+
 </head>
 <body>
+<div class="header">
+	<div class="mid">
+		ER IS GENOEG EDDY Wallypsum voor iedereen. waauw!
+	</div>
+</div>
+<div class="lampleft"></div>
+<div class="lampright"></div>
+<div class="lampbottom"></div>
+<div class="lamptop"></div>
 <div class="container">
 
-	<form method="post">
-		<div class="form-group">
-			<label for="slider">How many paragraphs do you want?</label>
-			<select type="select"  name="aantal">
-				<option value='1'>1</option>
-				<option value='2'>2</option>
-				<option value='3'>3</option>
-				<option value='4'>4</option>
-			</select>
-		</div>
+	<div class="form">
+		<form method="post">
+		<img src="assets/img/wow.gif" alt="" width="400px;">
+			<div class="form-group">
+				<label for="slider">Hoeveel geweldige paragrafen?</label>
+				<input type="text" name="select">
+			</div>
 
-		<div class="form-group">
-			<label for="slider">Maak je tekst helemaal</label>
-			<select type="select"  name="helemaal">
-				<option value='1'>1</option>
-				<option value='2'>2</option>
-				<option value='3'>3</option>
-				<option value='4'>4</option>
-			</select>
-						<input type="submit" name="submit" value="Get Excerpt">
+			<div class="form-group" style="display: none;">
+				<label for="slider">Maak je tekst helemaal</label>
+				<select type="select"  name="helemaal">
+					<option value='1'>1</option>
+					<option value='2'>2</option>
+					<option value='3'>3</option>
+					<option value='4'>4</option>
+				</select>
+							<input type="submit" name="submit" value="Get Excerpt">
 
-		</div>
-		
-	</form>
-		<?php
-			if (isset($_POST['aantal'])) {
-				$i = $_POST['aantal'];
-				while ($i--) {
-					$id = rand(1, $max_main);
-					foreach($json->long as $item) {
-						if ($item->id == $id) {
-							$ptag = '<p>' . $item->paragraph. '</p>';
-							echo '<p>';
-							echo htmlentities($ptag);
-							echo '</p>';
+			</div>
+			<input type="submit" class ="btn"name="submit" value="EDDY’S MAGISCHE KNOP">
+			
+		</form>
+	</div>
+	<div class="result">
+		<div class="innerresult">
+			<?php
+				if (isset($_POST['select'])) {
+					$i = $_POST['select'];
+					while ($i--) {
+						$id = rand(1, $max_main);
+						foreach($json->long as $item) {
+							if ($item->id == $id) {
+								$ptag = '<p>' . $item->paragraph. '</p>';
+
+								echo ($ptag);
+
+							}
 						}
 					}
 				}
-			}
-		?>
-
+			?>
+		</div>
+	</div>
 </div>
 
 </body>
